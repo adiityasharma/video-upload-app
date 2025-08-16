466f2e43702f17ec9393ac94b5a4b17a0b90411e
Aditya Sharma <adityasharma626367@gmail.com>
2025-08-16 13:39:32 +0530

    db connection

---
+++ app/lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!
+
+if (!MONGODB_URI) {
+  throw new Error("Please define mongo_uri in env variables")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {conn: null, promise:null}
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, { bufferCommands: false, maxPoolSize: 5 })
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
